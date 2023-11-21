495babac63c61485c3257d968201edd8e8902158
benjamin-contreas <becontreras@alumnos.uai.cl>
2023-11-20 22:00:01 -0300

    Se agrega Bootstrap

--- Proyecto/Carrito-Compras/src/pages/Home.jsx
+++ Proyecto/Carrito-Compras/src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      {/* Agregar nav bar */}
+      <div className='container-fluid text-center bg-primary '>
+        <h1>Bienvenidos a Nuestra Tienda</h1>
+      </div>
+    </div>
   )
 }
 
